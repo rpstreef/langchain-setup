e5ef6c7cc7069f24f7c391b2683cedeedf621035
Rolf Streefkerk <r.streefkerk@gmail.com>
2024-06-07 08:05:36 +0700

    feat: added vbox optional start

--- ssh_into_guest.sh
+++ ssh_into_guest.sh
@@ -3,6 +3,27 @@
 # We deploy to the 'default' machine, if you have more machines, replace the machine name here.
 machine_name="langflow"
 
+# Check if the VM is running
+VBoxManage list runningvms | grep -q "\"$machine_name\""
+if [ $? -eq 0 ]; then
+  echo "VM is running"
+else
+  echo "VM is not running"
+  VBoxManage startvm "$machine_name" --type headless
+fi
+
+# Wait for the VM to be fully booted
+while true; do
+  VBoxManage guestproperty get "$machine_name" "/VirtualBox/GuestInfo/OS/LoggedInUsers" | grep -q "0"
+  if [ $? -eq 0 ]; then
+    echo "VM is fully booted"
+    break
+  else
+    echo "Waiting for VM to boot..."
+    sleep 5
+  fi
+done
+
 # Using VirtualBox CLI to get the bridged network IP address
 guest_machine_ip=$(VBoxManage guestproperty get "$machine_name" "/VirtualBox/GuestInfo/Net/1/V4/IP" | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')
 
